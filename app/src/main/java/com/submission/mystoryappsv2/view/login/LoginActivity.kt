cad453edc77c47a58448ed295e93036518601ebe
Dewa899 <90895658+Dewa899@users.noreply.github.com>
2024-06-06 21:43:56 +0700

    final commit for submission 1

--- app/src/main/java/com/submission/mystoryappsv2/view/login/LoginActivity.kt
+++ app/src/main/java/com/submission/mystoryappsv2/view/login/LoginActivity.kt
@@ -8,15 +8,18 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.ProgressBar
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.submission.mystoryappsv2.R
 import com.submission.mystoryappsv2.data.pref.UserPreference
 import com.submission.mystoryappsv2.data.pref.dataStore
 import com.submission.mystoryappsv2.view.ViewModelFactory
 import com.submission.mystoryappsv2.databinding.ActivityLoginBinding
 import com.submission.mystoryappsv2.view.main.MainActivity
+import com.submission.mystoryappsv2.view.signup.SignupActivity
 import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
@@ -24,14 +27,17 @@ class LoginActivity : AppCompatActivity() {
         ViewModelFactory.getInstance(this)
     }
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var userPreference: UserPreference 
-
+    private lateinit var userPreference: UserPreference
+    private lateinit var progressBar: ProgressBar
+    private var isProcessing = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         userPreference = UserPreference.getInstance(dataStore)
+        progressBar = findViewById(R.id.progress_bar)
+        progressBar.visibility = View.GONE
 
         lifecycleScope.launch {
             userPreference.getSession().collect { user ->
@@ -44,8 +50,6 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
-
-
     }
     private fun navigateToStoryList() {
         val intent = Intent(this, MainActivity::class.java)
@@ -68,33 +72,48 @@ class LoginActivity : AppCompatActivity() {
 
     private fun setupAction() {
         binding.loginButton.setOnClickListener {
-            val email = binding.emailEditText.text.toString()
-            val password = binding.passwordEditText.text.toString()
-            viewModel.login(email, password) { success, message ->
-                if (success) {
-                    AlertDialog.Builder(this).apply {
-                        setTitle("Yeah!")
-                        setMessage("Anda berhasil login. Sudah tidak sabar untuk belajar ya?")
-                        setPositiveButton("Lanjut") { _, _ ->
-                            navigateToStoryList()
+            if (!isProcessing) {
+                isProcessing = true
+                binding.loginButton.isEnabled = false
+                binding.progressBar.visibility = View.VISIBLE
+
+                val email = binding.emailEditText.text.toString()
+                val password = binding.passwordEditText.text.toString()
+                viewModel.login(email, password) { success, message ->
+                    binding.progressBar.visibility = View.GONE
+                    isProcessing = false
+                    binding.loginButton.isEnabled = true
+
+                    if (success) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        AlertDialog.Builder(this).apply {
+                            setTitle("Login Gagal")
+                            setMessage("Email atau Password salah: $message")
+                            setPositiveButton("OK", null)
+                            create()
+                            show()
                         }
-                        create()
-                        show()
-                    }
-                } else {
-                    AlertDialog.Builder(this).apply {
-                        setTitle("Login Gagal")
-                        setMessage(message)
-                        setPositiveButton("OK", null)
-                        create()
-                        show()
                     }
                 }
+            }
+        }
+        binding.signupButton.setOnClickListener {
+            if (!isProcessing) {
+                val intent = Intent(this, SignupActivity::class.java)
+                startActivity(intent)
             }
         }
     }
 
     private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
 
         val title = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(100)
         val message =
@@ -108,6 +127,8 @@ class LoginActivity : AppCompatActivity() {
         val passwordEditTextLayout =
             ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(100)
         val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(100)
+            ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(100)
+        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(100)
 
         AnimatorSet().apply {
             playSequentially(
@@ -117,10 +138,12 @@ class LoginActivity : AppCompatActivity() {
                 emailEditTextLayout,
                 passwordTextView,
                 passwordEditTextLayout,
-                login
+                login,
+                signup
             )
             startDelay = 100
         }.start()
     }
+
 
 }
